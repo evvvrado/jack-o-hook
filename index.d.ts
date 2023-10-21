d12a1695e6c4bfc55fe116e4aba7897b2fb475c4
Everaldo Júnior <everaldocrj@gmail.com>
2023-10-21 15:50:20 -0300

    fix: scrollTop issue and add scrollPositionHook

--- index.d.ts
+++ index.d.ts
@@ -1,28 +1,30 @@
-import * as React from "react"
+import * as React from "react";
 
 declare module "jack-o-hook" {
-	type CopyboardHandler = (content: string, callback: () => void) => void
+	type CopyboardHandler = (content: string, callback: () => void) => void;
 
 	export function useCopyboard(): {
-		handleCopy: CopyboardHandler
-	}
+		handleCopy: CopyboardHandler;
+	};
 
 	export function useScroll(): {
-		goToId: (id: string) => void
-		goToTop: () => void
-	}
+		goToId: (id: string) => void;
+		goToTop: () => void;
+	};
+
+	export function useScrollPosition(): [boolean, boolean];
 
 	export function useMouse(): {
-		x: number
-		y: number
-	}
+		x: number;
+		y: number;
+	};
 
-	export function useWindowWidth(): number
+	export function useWindowWidth(): number;
 
-	export function useLockScroll(): void
+	export function useLockScroll(): void;
 
 	export function useIntersectionObserver<T extends HTMLElement>(
 		ref: React.RefObject<T>,
 		options?: IntersectionObserverInit
-	): boolean
+	): boolean;
 }
--- index.js
+++ index.js
@@ -1,83 +1,105 @@
-import * as React from "react"
-
-function getEnvironment() {
-	const isDOM =
-		typeof window !== "undefined" && window.document && window.document.documentElement
-
-	return isDOM ? "browser" : "server"
-}
+import * as React from "react";
 
 const useCopyboard = () => {
 	const handleCopy = React.useCallback((content, callback) => {
-		navigator.clipboard.writeText(content)
-		callback()
-	})
+		navigator.clipboard.writeText(content);
+		callback();
+	});
 
-	return { handleCopy }
-}
-
+	return { handleCopy };
+};
 
 const useScroll = () => {
 	const goToId = React.useCallback((id) => {
-		const elementID = document.querySelector(`#${id}`)
-		if (!elementID) return
+		const elementID = document.querySelector(`#${id}`);
+		if (!elementID) return;
 
-		const elementBounds = elementID?.getBoundingClientRect()
-		return window.scrollTo(0, (window.scrollY + elementBounds.y - 150) || 0)
-	}, [])
+		const elementBounds = elementID?.getBoundingClientRect();
+		return window.scrollTo(0, window.scrollY + elementBounds.y - 150 || 0);
+	}, []);
 
 	const goToTop = React.useCallback(() => {
-		return window.scrollTo(0)
-	}, [])
+		return window.scrollTo(0, 0);
+	}, []);
 
+	return { goToId, goToTop };
+};
 
-	return { goToId, goToTop }
-}
+const useScrollPosition = () => {
+	const [isScrolling, setIsScrolling] = React.useState(false);
+	const [isOnTop, setIsOnTop] = React.useState(false);
+
+	React.useEffect(() => {
+		let offSet = 0;
+
+		const scrollCheck = () => {
+			if (window) {
+				if (offSet > window.scrollY || window.scrollY == 0) {
+					setIsOnTop(false);
+					setIsScrolling(true);
+				} else {
+					setIsOnTop(true);
+					setIsScrolling(false);
+				}
+
+				offSet = window.scrollY;
+				offSet === 0 ? setIsScrolling(false) : setIsScrolling(true);
+			}
+		};
+
+		scrollCheck();
+		window.addEventListener("scroll", scrollCheck);
+
+		return () => window.removeEventListener("scroll", scrollCheck);
+	}, []);
+
+	return [isScrolling, isOnTop];
+};
 
 const useMouse = () => {
 	const [mousePos, setMousePos] = React.useState({
 		x: 0,
 		y: 0,
-	})
+	});
 
 	const handleMouseMove = (event) => {
-		setMousePos({ x: event.clientX, y: event.clientY })
-	}
+		setMousePos({ x: event.clientX, y: event.clientY });
+	};
 
 	useEffect(() => {
-		window.addEventListener("mousemove", handleMouseMove)
-		return () => window.removeEventListener("mousemove", handleMouseMove)
-	}, [mousePos])
+		window.addEventListener("mousemove", handleMouseMove);
+		return () => window.removeEventListener("mousemove", handleMouseMove);
+	}, [mousePos]);
 
-	return mousePos
-}
+	return mousePos;
+};
 
 const useWindowWidth = () => {
-	const [width, setWidth] = React.useState(0)
-	const handleResize = () => setWidth(window.innerWidth)
+	const [width, setWidth] = React.useState(0);
+	const handleResize = () => setWidth(window.innerWidth);
 
 	React.useEffect(() => {
-		handleResize()
-	}, [])
+		handleResize();
+	}, []);
 
 	React.useEffect(() => {
-		window.addEventListener("resize", handleResize)
-		return () => window.removeEventListener("resize", handleResize)
-	}, [width])
+		window.addEventListener("resize", handleResize);
+		return () => window.removeEventListener("resize", handleResize);
+	}, [width]);
 
-	return width
-}
+	return width;
+};
 
 const useLockScroll = () => {
 	React.useEffect(() => {
-		const initialStyle = window.getComputedStyle(document.body).overflow
-		document.body.style.overflow = "hidden"
+		const initialStyle = window.getComputedStyle(document.body).overflow;
+		document.body.style.overflow = "hidden";
 
 		return () => {
-			document.body.style.overflow = initialStyle
-		}
-	}, [])
-}
+			document.body.style.overflow = initialStyle;
+		};
+	}, []);
+};
 
 const useIntersectionObserver = (ref, options) => {
 	const [isIntersecting, setIsIntersecting] = React.useState(false);
@@ -99,4 +121,12 @@ const useIntersectionObserver = (ref, options) => {
 	return isIntersecting;
 };
 
-export { useCopyboard, useMouse, useScroll, useWindowWidth, useLockScroll, useIntersectionObserver }
+export {
+	useCopyboard,
+	useMouse,
+	useScroll,
+	useScrollPosition,
+	useWindowWidth,
+	useLockScroll,
+	useIntersectionObserver,
+};
